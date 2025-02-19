fe68917531063fa40f5cde88fbfe709e529f3375
seannn9 <naeskirlu9@gmail.com>
2025-02-19 10:21:57 +0800

    completed welecome page content. buttons not functional so far

--- client/src/pages/Welcome.jsx
+++ client/src/pages/Welcome.jsx
@@ -5,7 +5,19 @@ export default function Welcome() {
     return (
         <div className="welcome-container">
             <Navbar />
-            <div className="welcome-section">Welcome</div>
+            <section className="welcome-section">
+                <div className="welcome-msg">
+                    <h1>
+                        <strong>The Perfect Seat.</strong>
+                    </h1>
+                    <h1>Just A Click Away</h1>
+                    <p>
+                        Skip the lines and reserve your seat online. <br />
+                        Enjoy your movie, stress-free!
+                    </p>
+                </div>
+                <button className="welcome-btn">Get Started</button>
+            </section>
         </div>
     );
 }
