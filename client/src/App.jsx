7bdf3c289eda78ec31939cec53d69604369a6aec
seannn9 <naeskirlu9@gmail.com>
2025-02-19 21:31:35 +0800

    global fontawesome

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import "./styles/App.css";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
 // Pages
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Welcome from "./pages/Welcome";
+
+library.add(fas);
 
 function App() {
     return (
