4867a453ca7c670f73ed6acd9dcc81bfd48dad1a
seannn9 <naeskirlu9@gmail.com>
2025-02-19 10:22:29 +0800

    added logo

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,20 @@
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
+import logo from "../assets/favicon32.png";
 
 export default function Navbar() {
     return (
         <nav className="navbar">
             <div className="nav-logo">
                 <Link to="/">
-                    <img src="" alt="logo" />
+                    <img src={logo} alt="logo" />
+                    <h1>FilmReserve</h1>
                 </Link>
             </div>
             <div className="nav-links">
-                <Link to="/login">Login</Link>
+                <Link to="/login" className="login-btn">
+                    Login
+                </Link>
             </div>
         </nav>
     );
