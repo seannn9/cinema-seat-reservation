09092210c0bc3e47f6f78c646801a1029ae42508
seannn9 <naeskirlu9@gmail.com>
2025-02-20 15:10:06 +0800

    using useNavigate state prop to pass in props to MovieDetails page

--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -1,8 +1,23 @@
 import "../styles/MovieCard.css";
+import { useNavigate } from "react-router-dom";
 
 export default function MovieCard(props) {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate("/movie-details", {
+            state: {
+                image: props.image,
+                title: props.title,
+                release: props.release,
+                duration: props.duration,
+                genre: props.genre,
+            },
+        });
+    };
+
     return (
-        <div className="movie-card">
+        <div className="movie-card" onClick={handleClick}>
             <div className="movie-poster">
                 <img src={props.image} alt={props.title} />
             </div>
