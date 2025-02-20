0f5041ed97de29dd1ff2a81d75ad9d4634c778f4
seannn9 <naeskirlu9@gmail.com>
2025-02-20 11:19:50 +0800

    year is not in a new line anymore

--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -7,8 +7,10 @@ export default function MovieCard(props) {
                 <img src={props.image} alt={props.title} />
             </div>
             <div className="movie-details">
-                <h4>{props.title}</h4>
-                <p>{props.release}</p>
+                <h4>
+                    {props.title} (
+                    <span style={{ color: "#bababa" }}>{props.release}</span>)
+                </h4>
             </div>
         </div>
     );
