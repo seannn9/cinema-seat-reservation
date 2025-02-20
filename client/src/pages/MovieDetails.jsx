1b9f3bef9bf7844c79134e374a140a777b345d62
seannn9 <naeskirlu9@gmail.com>
2025-02-20 20:43:35 +0800

    styling changes

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -9,7 +9,7 @@ export default function MovieDetails() {
     const [selectedMall, setSelectedMall] = useState("Select Theater");
     const [selectedDate, setSelectedDate] = useState("Select Date");
     const [selectedTime, setSelectedTime] = useState("Select Time");
-
+    const [price, setPrice] = useState(0);
     return (
         <div className="movie-details-container">
             <Navbar />
@@ -21,20 +21,20 @@ export default function MovieDetails() {
                     >
                         <h2>Theater</h2>
                         <div className="theater-list">
-                            <h3 onClick={() => setSelectedMall("SM Trece")}>
+                            <h4 onClick={() => setSelectedMall("SM Trece")}>
                                 <FontAwesomeIcon icon="fa-solid fa-location-dot" />
                                 &nbsp; SM Trece
-                            </h3>
-                            <h3 onClick={() => setSelectedMall("SM Dasma")}>
+                            </h4>
+                            <h4 onClick={() => setSelectedMall("SM Dasma")}>
                                 {" "}
                                 <FontAwesomeIcon icon="fa-solid fa-location-dot" />
                                 &nbsp; SM Dasma
-                            </h3>
-                            <h3 onClick={() => setSelectedMall("SM Tanza")}>
+                            </h4>
+                            <h4 onClick={() => setSelectedMall("SM Tanza")}>
                                 {" "}
                                 <FontAwesomeIcon icon="fa-solid fa-location-dot" />
                                 &nbsp; SM Tanza
-                            </h3>
+                            </h4>
                         </div>
                     </div>
                     <div
@@ -43,18 +43,18 @@ export default function MovieDetails() {
                     >
                         <h2>Date</h2>
                         <div className="date-list">
-                            <h3 onClick={() => setSelectedDate("Feb 21")}>
+                            <h4 onClick={() => setSelectedDate("Feb 21")}>
                                 Feb 21
-                            </h3>
-                            <h3 onClick={() => setSelectedDate("Feb 22")}>
+                            </h4>
+                            <h4 onClick={() => setSelectedDate("Feb 22")}>
                                 Feb 22
-                            </h3>
-                            <h3 onClick={() => setSelectedDate("Feb 23")}>
+                            </h4>
+                            <h4 onClick={() => setSelectedDate("Feb 23")}>
                                 Feb 23
-                            </h3>
-                            <h3 onClick={() => setSelectedDate("Feb 24")}>
+                            </h4>
+                            <h4 onClick={() => setSelectedDate("Feb 24")}>
                                 Feb 24
-                            </h3>
+                            </h4>
                         </div>
                     </div>
                     <div
@@ -63,18 +63,18 @@ export default function MovieDetails() {
                     >
                         <h2>Time</h2>
                         <div className="time-list">
-                            <h3 onClick={() => setSelectedTime("10:30 AM")}>
+                            <h4 onClick={() => setSelectedTime("10:30 AM")}>
                                 10:30 AM
-                            </h3>
-                            <h3 onClick={() => setSelectedTime("12:00 PM")}>
+                            </h4>
+                            <h4 onClick={() => setSelectedTime("12:00 PM")}>
                                 12:00 PM
-                            </h3>
-                            <h3 onClick={() => setSelectedTime("1:30 PM")}>
+                            </h4>
+                            <h4 onClick={() => setSelectedTime("1:30 PM")}>
                                 1:30 PM
-                            </h3>
-                            <h3 onClick={() => setSelectedTime("3:00 PM")}>
+                            </h4>
+                            <h4 onClick={() => setSelectedTime("3:00 PM")}>
                                 3:00 PM
-                            </h3>
+                            </h4>
                         </div>
                     </div>
                 </div>
@@ -96,6 +96,9 @@ export default function MovieDetails() {
                         <h2>{selectedMall}</h2>
                         <h3>{selectedDate}</h3>
                         <p>{selectedTime}</p>
+                        <p style={{ textAlign: "right" }}>
+                            Total Price: {price}
+                        </p>
                         <button>Proceed to Payment</button>
                     </div>
                 </div>
