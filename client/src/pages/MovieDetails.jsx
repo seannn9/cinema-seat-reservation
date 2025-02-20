6f6303553fc5c5c0d30c8aeb46eadfbfceb0645d
seannn9 <naeskirlu9@gmail.com>
2025-02-20 20:06:07 +0800

    styling changes

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import "../styles/MovieDetails.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function MovieDetails() {
     const { state } = useLocation();
+    const [selectedMall, setSelectedMall] = useState("Select Theater");
+    const [selectedDate, setSelectedDate] = useState("Select Date");
+    const [selectedTime, setSelectedTime] = useState("Select Time");
 
     return (
         <div className="movie-details-container">
@@ -16,9 +21,20 @@ export default function MovieDetails() {
                     >
                         <h2>Theater</h2>
                         <div className="theater-list">
-                            <h3>SM Trece</h3>
-                            <h3>SM Dasma</h3>
-                            <h3>SM Tanza</h3>
+                            <h3 onClick={() => setSelectedMall("SM Trece")}>
+                                <FontAwesomeIcon icon="fa-solid fa-location-dot" />
+                                &nbsp; SM Trece
+                            </h3>
+                            <h3 onClick={() => setSelectedMall("SM Dasma")}>
+                                {" "}
+                                <FontAwesomeIcon icon="fa-solid fa-location-dot" />
+                                &nbsp; SM Dasma
+                            </h3>
+                            <h3 onClick={() => setSelectedMall("SM Tanza")}>
+                                {" "}
+                                <FontAwesomeIcon icon="fa-solid fa-location-dot" />
+                                &nbsp; SM Tanza
+                            </h3>
                         </div>
                     </div>
                     <div
@@ -27,10 +43,18 @@ export default function MovieDetails() {
                     >
                         <h2>Date</h2>
                         <div className="date-list">
-                            <h3>Date 1</h3>
-                            <h3>Date 2</h3>
-                            <h3>Date 3</h3>
-                            <h3>Date 4</h3>
+                            <h3 onClick={() => setSelectedDate("Feb 21")}>
+                                Feb 21
+                            </h3>
+                            <h3 onClick={() => setSelectedDate("Feb 22")}>
+                                Feb 22
+                            </h3>
+                            <h3 onClick={() => setSelectedDate("Feb 23")}>
+                                Feb 23
+                            </h3>
+                            <h3 onClick={() => setSelectedDate("Feb 24")}>
+                                Feb 24
+                            </h3>
                         </div>
                     </div>
                     <div
@@ -39,10 +63,18 @@ export default function MovieDetails() {
                     >
                         <h2>Time</h2>
                         <div className="time-list">
-                            <h3>Time 1</h3>
-                            <h3>Time 2</h3>
-                            <h3>Time 3</h3>
-                            <h3>Time 4</h3>
+                            <h3 onClick={() => setSelectedTime("10:30 AM")}>
+                                10:30 AM
+                            </h3>
+                            <h3 onClick={() => setSelectedTime("12:00 PM")}>
+                                12:00 PM
+                            </h3>
+                            <h3 onClick={() => setSelectedTime("1:30 PM")}>
+                                1:30 PM
+                            </h3>
+                            <h3 onClick={() => setSelectedTime("3:00 PM")}>
+                                3:00 PM
+                            </h3>
                         </div>
                     </div>
                 </div>
@@ -61,9 +93,9 @@ export default function MovieDetails() {
                         </div>
                     </div>
                     <div className="order-confirmation">
-                        <h2>SM Trece</h2>
-                        <h3>Feb 21, 2025</h3>
-                        <p>12:00pm</p>
+                        <h2>{selectedMall}</h2>
+                        <h3>{selectedDate}</h3>
+                        <p>{selectedTime}</p>
                         <button>Proceed to Payment</button>
                     </div>
                 </div>
