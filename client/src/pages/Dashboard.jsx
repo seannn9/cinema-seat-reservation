fa250f3e4008533695bab586786c222ead9a5556
seannn9 <naeskirlu9@gmail.com>
2025-02-20 23:36:36 +0800

    fetch from movies database instead of manually adding movies

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,16 +1,13 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Axios from "axios";
 import Navbar from "../components/Navbar";
 import "../styles/Dashboard.css";
 import MovieCard from "../components/MovieCard";
-// dummy movie posters
-import ca4 from "../assets/movies/ca4.jpg";
-import sonic3 from "../assets/movies/sonic3.jpg";
-import flow from "../assets/movies/flow.jpg";
-import monkey from "../assets/movies/monkey.jpg";
 
 export default function Dashboard() {
     const navigate = useNavigate();
+    const [movies, setMovies] = useState([]);
 
     useEffect(() => {
         const userid = localStorage.getItem("userid");
@@ -21,44 +18,29 @@ export default function Dashboard() {
         }
     }, []);
 
+    useEffect(() => {
+        Axios.post("http://localhost:5001/getmovie").then((response) => {
+            setMovies(response.data);
+        });
+    }, []);
+
     return (
         <div className="dashboard-container">
             <Navbar />
             <section className="dashboard-section">
                 <h1>Now Showing</h1>
                 <div className="movies">
-                    <MovieCard
-                        image={ca4}
-                        title="Captain America: Brave New World"
-                        release="2025"
-                        duration="1h 59m"
-                        genre="Action, Thriller, Science Fiction"
-                        price={320}
-                    />
-                    <MovieCard
-                        image={sonic3}
-                        title="Sonic the Hedgehog 3"
-                        release="2025"
-                        duration="1h 50m"
-                        genre="Action, Comedy, Science Fiction"
-                        price={320}
-                    />
-                    <MovieCard
-                        image={flow}
-                        title="Flow"
-                        release="2025"
-                        duration="1h 25m"
-                        genre="Animation, Fantasy, Adventure"
-                        price={320}
-                    />
-                    <MovieCard
-                        image={monkey}
-                        title="The Monkey"
-                        release="2025"
-                        duration="1h 38m"
-                        genre="Horror, Comedy"
-                        price={320}
-                    />
+                    {movies.map((movie, key) => (
+                        <MovieCard
+                            key={key}
+                            image={movie.poster}
+                            title={movie.title}
+                            release={movie.release_date}
+                            duration={movie.duration}
+                            genre={movie.genre}
+                            price={movie.price}
+                        />
+                    ))}
                 </div>
             </section>
         </div>
