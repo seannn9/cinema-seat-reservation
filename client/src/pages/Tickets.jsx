630a9cb1d739f20177f4aebcfc9250b788453b22
seannn9 <naeskirlu9@gmail.com>
2025-02-21 16:38:17 +0800

    unfinished page but already shows the correct tickets for the user

--- client/src/pages/Tickets.jsx
+++ client/src/pages/Tickets.jsx
@@ -1,3 +1,51 @@
+import Axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Navbar from "../components/Navbar";
+
 export default function Tickets() {
-    return <div>Tickets</div>;
+    const [tickets, setTickets] = useState([]);
+    const [username, setUsername] = useState("");
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const userid = localStorage.getItem("userid");
+        if (userid) {
+            console.log("Logged in");
+
+            setUsername(localStorage.getItem("username"));
+
+            Axios.post("http://localhost:5001/getusertickets", {
+                userid: localStorage.getItem("userid"),
+            }).then((response) => {
+                setTickets(response.data);
+            });
+
+            if (tickets.length === 0) {
+                console.log("No tickets found");
+            }
+        } else {
+            navigate("/login");
+        }
+    }, []);
+
+    return (
+        <div className="tickets-container">
+            <Navbar />
+            <div className="tickets-section">
+                <h1>{username}'s Tickets</h1>
+                <div className="tickets">
+                    {tickets.map((ticket, key) => (
+                        <div className="ticket" key={key}>
+                            <h1>{ticket.movie}</h1>
+                            <p>{ticket.location}</p>
+                            <p>{ticket.date}</p>
+                            <p>{ticket.time}</p>
+                            <p>₱{ticket.price}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
 }
