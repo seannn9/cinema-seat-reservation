d0149010022c9cc26a6861cbf295a157810ff672
seannn9 <naeskirlu9@gmail.com>
2025-02-26 23:08:48 +0800

    set up grid layout for payment page

--- client/src/pages/Payment.jsx
+++ client/src/pages/Payment.jsx
@@ -2,11 +2,11 @@ import Navbar from "../components/Navbar";
 import { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import Axios from "axios";
+import "../styles/Payment.css";
 
 export default function PaymentComplete() {
     const navigate = useNavigate();
     const { state } = useLocation();
-    // payment variables
     const [paymentStatus, setPaymentStatus] = useState("");
     const [isProcessing, setIsProcessing] = useState(false);
 
@@ -52,8 +52,16 @@ export default function PaymentComplete() {
         <div className="payment-container">
             <Navbar />
             <section className="payment-section">
-                <button onClick={handlePayment}>Pay Now</button>
                 {isProcessing ? <p>Processing...</p> : <p>{paymentStatus}</p>}
+                <div className="payment-method">
+                    <h1>Payment Method</h1>
+                </div>
+                <div className="order-summary">
+                    <h2>Booking Detail</h2>
+                </div>
+                <div className="payment">
+                    <button onClick={handlePayment}>Pay Now</button>
+                </div>
             </section>
         </div>
     );
