299784f085c82d2ca044aa4f28ea8a6e3f5e0cac
seannn9 <naeskirlu9@gmail.com>
2025-03-04 10:36:23 +0800

    added payment methods, not working yet

--- client/src/pages/Payment.jsx
+++ client/src/pages/Payment.jsx
@@ -9,6 +9,7 @@ export default function PaymentComplete() {
     const { state } = useLocation();
     const [paymentStatus, setPaymentStatus] = useState("");
     const [isProcessing, setIsProcessing] = useState(false);
+    const [paymentMethod, setPaymentMethod] = useState("");
 
     useEffect(() => {
         const userid = localStorage.getItem("userid");
@@ -18,6 +19,15 @@ export default function PaymentComplete() {
             navigate("/login");
         }
     }, []);
+
+    useEffect(() => {
+        const movieToPay = localStorage.getItem("movieToPay");
+        if (movieToPay) {
+            console.log(movieToPay);
+        } else {
+            navigate("/dashboard");
+        }
+    });
 
     const handlePayment = () => {
         if (!isProcessing) {
@@ -52,12 +62,90 @@ export default function PaymentComplete() {
         <div className="payment-container">
             <Navbar />
             <section className="payment-section">
-                {isProcessing ? <p>Processing...</p> : <p>{paymentStatus}</p>}
+                {isProcessing && <p>Processing...</p>}
+
                 <div className="payment-method">
-                    <h1>Payment Method</h1>
+                    <h2>Payment Method</h2>
+                    <div className="methods">
+                        <h4
+                            onClick={() => setPaymentMethod("Credit")}
+                            className={
+                                paymentMethod === "Credit" ? "active" : ""
+                            }
+                        >
+                            Credit Card
+                        </h4>
+                        <h4
+                            onClick={() => setPaymentMethod("Bank")}
+                            className={paymentMethod === "Bank" ? "active" : ""}
+                        >
+                            Bank Transfer
+                        </h4>
+                        <h4
+                            onClick={() => setPaymentMethod("Gcash")}
+                            className={
+                                paymentMethod === "Gcash" ? "active" : ""
+                            }
+                        >
+                            Gcash
+                        </h4>
+                    </div>
+                    <div className="user-info">
+                        <h4 className="payment-labels">Name</h4>
+                        <input
+                            type="text"
+                            id="fullname"
+                            className="payment-inputs"
+                        />
+                    </div>
+                    {paymentMethod === "Gcash" && (
+                        <div>
+                            <h4 className="payment-labels">Gcash Number</h4>
+                            <input
+                                type="tel"
+                                id="gcashnum"
+                                className="payment-inputs"
+                            />
+                        </div>
+                    )}
+                    {paymentMethod === "Bank" && (
+                        <div>
+                            <h4 className="payment-labels">
+                                Bank Account Number
+                            </h4>
+                            <input
+                                type="tel"
+                                id="banknum"
+                                className="payment-inputs"
+                            />
+                        </div>
+                    )}
+                    {paymentMethod === "Credit" && (
+                        <div>
+                            <h4 className="payment-labels">
+                                Credit Card Number
+                            </h4>
+                            <input
+                                type="tel"
+                                id="creditnum"
+                                className="payment-inputs"
+                            />
+                            <h4 className="payment-labels">CVV</h4>
+                            <input
+                                type="num"
+                                id="cvv"
+                                className="payment-inputs"
+                            />
+                        </div>
+                    )}
                 </div>
                 <div className="order-summary">
                     <h2>Booking Detail</h2>
+                    <h3>Schedule</h3>
+                    <p>Movie Title</p>
+                    <h3>Title</h3>
+                    <p>Date</p>
+                    <h3>Date</h3>
                 </div>
                 <div className="payment">
                     <button onClick={handlePayment}>Pay Now</button>
